12d6666605833b0a6c4bc736d4d4c217ba41924d
Idris <i.barahanov@mail.ru>
2025-10-29 21:18:25 +0300

    first

---
+++ pkg/api/nextdate.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"fmt"
+	"go1f/pkg/dateutils"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func NextDayHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Debug: now=%q, date=%q, repeat=%q\n",
+		r.URL.Query().Get("now"),
+		r.URL.Query().Get("date"),
+		r.URL.Query().Get("repeat"))
+
+	nowVol := strings.TrimSpace(r.URL.Query().Get("now"))
+	dateVol := strings.TrimSpace(r.URL.Query().Get("date"))
+	repeat := strings.TrimSpace(r.URL.Query().Get("repeat"))
+
+	now, err := time.Parse("20060102", nowVol)
+	if err != nil {
+		http.Error(w, "ошибка формата now", http.StatusBadRequest)
+		return
+	}
+
+	result, err := dateutils.NextDate(now, dateVol, repeat)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprint(w, result)
+}
