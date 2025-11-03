4d67fd9d390b9fa4754a5af28e8f67f3689b0835
Idris <i.barahanov@mail.ru>
2025-11-03 18:46:15 +0300

    123

--- pkg/api/addtask.go
+++ pkg/api/addtask.go
@@ -2,10 +2,8 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"go1f/pkg/dateutils"
 	"go1f/pkg/db"
-	_ "modernc.org/sqlite"
 	"net/http"
 	"time"
 )
@@ -16,57 +14,55 @@ func checkDate(task *db.Task) error {
 	if task.Date == "" {
 		task.Date = now.Format("20060102")
 	}
+
 	t, err := time.Parse("20060102", task.Date)
 	if err != nil {
 		return err
 	}
-	var next string
 
 	if task.Repeat != "" {
-		next, err = dateutils.NextDate(now, task.Date, task.Repeat)
+		next, err := dateutils.NextDate(now, task.Date, task.Repeat)
 		if err != nil {
 			return err
 		}
-	}
-	if afterNow(now, t) {
-		if len(task.Repeat) == 0 {
-			task.Date = now.Format("20060102")
-		} else {
+		if afterNow(now, t) {
 			task.Date = next
 		}
+	} else if afterNow(now, t) {
+		task.Date = now.Format("20060102")
 	}
+
 	return nil
 }
 
 func addTaskHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("---- Incoming request ----")
-	defer func() {
-		fmt.Println("---- END of request ----")
-	}()
+	if r.Method != http.MethodPost {
+		http.Error(w, `{"error":"method not allowed"}`, http.StatusMethodNotAllowed)
+		return
+	}
 
 	var task db.Task
-	fmt.Printf("Response JSON: %+v\n", map[string]any{"id": task.ID})
-
 	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
-		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		http.Error(w, `{"error":"invalid json"}`, http.StatusBadRequest)
 		return
 	}
 
 	if task.Title == "" {
-		http.Error(w, "title is required", http.StatusBadRequest)
+		http.Error(w, `{"error":"title is required"}`, http.StatusBadRequest)
 		return
 	}
 
 	if err := checkDate(&task); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, `{"error":"`+err.Error()+`"}`, http.StatusBadRequest)
 		return
 	}
 
 	id, err := db.AddTask(&task)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, `{"error":"`+err.Error()+`"}`, http.StatusInternalServerError)
 		return
 	}
 
-	json.NewEncoder(w).Encode(map[string]any{"id": id})
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(map[string]any{"id": id})
 }
